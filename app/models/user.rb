556eadafdd769247fb4bb5eab133e0d43e58cfb1
John N. Mandalakas <j.n.mandalakas@gmail.com>
2015-02-26 16:54:36 -0600

    modify devise files to work with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
+
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 
   has_many :questions, foreign_key: :author_id
   has_many :answers, foreign_key: :author_id
@@ -15,4 +22,22 @@ class User < ActiveRecord::Base
   has_many :questions_voted_on, through: :votes, source: :questions
   has_many :answers_voted_on, through: :votes, source: :answers
   has_many :comments_voted_on, through: :votes, source: :comments
+
+  # For using username with devise
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
+  end
 end
