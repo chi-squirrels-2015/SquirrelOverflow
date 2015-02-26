d72fb1b993432c889936e872096ccc2d308439b0
Gabriela Voicu <gabrielavoicu16@gmail.com>
2015-02-26 14:33:20 -0600

    add Question controller
    add methods for all the routes (index, show, etc)

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,26 @@
+class Answers < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  # need to decide if we will implement this
+  def destroy
+
+  end
+end
---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,46 @@
+class Question < ApplicationController
+
+  def index
+    @questions = Question.recent
+  end
+
+  def show
+    @question = Question.find_by_id(params[:question_id])
+    @answers = @question.answers
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    question = Question.new(question_params)
+    if question.save
+      redirect_to action: "show", id: question.id
+    else
+      redirect_to action: "new"
+    end
+  end
+
+  def edit
+     @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update(question_params)
+  end
+
+  # need to decide if we will implement this
+  def destroy
+    Question.find(params[:id]).destroy
+    redirect_to "/questions}"
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :content).merge({user: current_user})
+  end
+
+end
