e03bf1782d68a2efd6e4392a580922a63d094ebd
Gabriela Voicu <gabrielavoicu16@gmail.com>
2015-02-26 17:23:48 -0600

    add GET #show tests to QuestionController spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 describe QuestionsController do
-  let!(:user) {User.create!(email: "fake@fake.com", password: "fakefake")}
-  # change user email to username when fixed in migrations
+  let!(:user) {User.create!(username: "fakeusername", email: "fake@fake.com", password: "fakefake")}
   let!(:question) {Question.create!(title:'fake question', content: 'fake', author: user)}
 
 
@@ -21,6 +20,22 @@ describe QuestionsController do
       get :index
       expect(assigns(:questions)).to eq(Question.recent)
     end
+  end
+
+  describe "GET #show" do
+
+    it "assigns @question to the current question" do
+      get :show, { id: question.to_param }
+      expect(assigns(:question)).to eq(question)
+    end
+
+    it "assigns @answers to the answers of the current question" do
+      get :show, { id: question.to_param }
+      expect(assigns(:answers)).to eq(question.answers)
+    end
+
+
+
   end
 
 end
