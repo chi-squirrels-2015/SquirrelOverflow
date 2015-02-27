097c1f75a4addd8c227114b76094eb8510047d26
Ian Agne <ian.d.agne@gmail.com>
2015-02-27 16:43:39 -0600

    modify answer controller, question show view to allow users to add answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,20 +6,13 @@ class AnswersController < ApplicationController
   end
 
   def create
+    @question = Question.find(params[:question_id])
     answer = Answer.new(answer_params)
-    answer.save
-    redirect_to "/#{answer.question}"
-    # respond_to do |format|
-    #     if answer.save
-    #       format.html { redirect_to answer.question, notice: 'Answer was successfully created.' }
-    #       format.js   {}
-    #       format.json { render json: answer, status: :created, location: answer }
-    #     else
-    #       # format.html { render action: "new" }
-    #       format.json { render json: answer.errors, status: :unprocessable_entity }
-    #     end
-    # end
-
+    if answer.save
+      redirect_to question_path(@question)
+    else
+      #some error handling here
+    end
   end
 
   def edit
