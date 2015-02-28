76de1428e2c069a2d73d46003c9477a4581a9757
Ian Agne <ian.d.agne@gmail.com>
2015-02-27 19:43:36 -0600

    add vote_params method to controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,12 +2,17 @@ class VotesController < ApplicationController
 
   def create
     @question = Question.find(params[:question_id])
+    vote = Vote.new(vote_params)
+
+    if vote.save
+      "horray!"
+    end
 
   end
 
   private
 
   def vote_params
-
+    params.require(:vote).permit(:content).merge({voter: current_user, question: @question})
   end
 end
